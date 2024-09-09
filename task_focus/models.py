d3d33fc1b6fc023d15322b6ec9138f293ec101a2
Stepan <oleksiukstepann@gmail.com>
2024-09-10 00:28:41 +0300

    create Position model

--- task_focus/models.py
+++ task_focus/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class TaskType(models.Model):
     name = models.CharField(max_length=100)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)

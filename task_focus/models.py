8e05b7742ca0147a1fcc064faac66a2ff2ef5032
Stepan <oleksiukstepann@gmail.com>
2024-09-10 00:31:07 +0300

    create Worker model as abstract user

--- task_focus/models.py
+++ task_focus/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 class TaskType(models.Model):
@@ -7,3 +8,10 @@ class TaskType(models.Model):
 
 class Position(models.Model):
     name = models.CharField(max_length=100)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
+

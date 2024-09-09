bbfe050bfd7be82a60340fffb934e1a245ae54cb
Stepan <oleksiukstepann@gmail.com>
2024-09-10 01:50:42 +0300

    add null=True and blank=True to Worker position field

--- task_focus/models.py
+++ task_focus/models.py
@@ -11,7 +11,12 @@ class Position(models.Model):
 
 
 class Worker(AbstractUser):
-    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
 
     class Meta:
         ordering = ["first_name"]

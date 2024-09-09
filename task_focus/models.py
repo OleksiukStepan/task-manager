be60cdac5b13b8663ac606f431c036531143e787
Stepan <oleksiukstepann@gmail.com>
2024-09-10 00:33:06 +0300

    create Task model

--- task_focus/models.py
+++ task_focus/models.py
@@ -14,4 +14,11 @@ class Worker(AbstractUser):
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
 
 
-
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_complete = models.BooleanField(default=False)
+    # priority = models.IntegerField(default=0)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="tasks")

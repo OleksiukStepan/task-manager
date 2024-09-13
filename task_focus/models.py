9eddd7a08ff335fa40762553340854799d483199
Stepan <oleksiukstepann@gmail.com>
2024-09-13 18:37:59 +0300

    add coices to Task model

--- task_focus/models.py
+++ task_focus/models.py
@@ -25,10 +25,12 @@ class Worker(AbstractUser):
 
 
 class Task(models.Model):
+    CHOICES = [(1, "Urgent"), (2, "High"), (3, "Medium"), (4, "Low")]
+
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     deadline = models.DateField()
     is_complete = models.BooleanField(default=False)
-    # priority = models.IntegerField(default=0)
+    priority = models.IntegerField(choices=CHOICES, default=3)
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
     assignees = models.ManyToManyField(Worker, related_name="tasks")

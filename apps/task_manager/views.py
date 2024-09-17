f2b7663cbe5dfbdbdcffef730167492f42c0d19a
Stepan <oleksiukstepann@gmail.com>
2024-09-17 23:53:36 +0300

    update view.py & add display for team_meambers in index.html

--- apps/task_manager/views.py
+++ apps/task_manager/views.py
@@ -1,18 +1,20 @@
-from datetime import datetime
-
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template import loader
 
-from apps.task_manager.models import Task
+from apps.task_manager.models import Task, Worker
 
 
 # @login_required(login_url="/login/")
 def index(request):
     recent_tasks = Task.objects.order_by("-created_at")[:5]
+    team_members = Worker.objects.all()[:5]
+
+
     context = {
         "segment": "index",
         "recent_tasks": recent_tasks,
+        "team_members": team_members,
     }
 
     html_template = loader.get_template("pages/index.html")

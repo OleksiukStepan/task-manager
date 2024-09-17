607189d4e169d21291a1dd4129e0dec3820ccda7
Stepan <oleksiukstepann@gmail.com>
2024-09-17 22:36:08 +0300

    add display of recently created tasks

--- apps/task_manager/views.py
+++ apps/task_manager/views.py
@@ -4,10 +4,16 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template import loader
 
+from apps.task_manager.models import Task
+
 
 # @login_required(login_url="/login/")
 def index(request):
-    context = {'segment': 'index'}
+    recent_tasks = Task.objects.order_by("-created_at")[:5]
+    context = {
+        "segment": "index",
+        "recent_tasks": recent_tasks,
+    }
 
-    html_template = loader.get_template('pages/index.html')
+    html_template = loader.get_template("pages/index.html")
     return HttpResponse(html_template.render(context, request))

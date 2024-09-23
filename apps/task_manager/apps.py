a949445d2b65870027ec831947e741d97b5ccd71
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2024-09-23 22:15:30 +0300

    Revert "Implement Task Manager for creating, managing, and assigning tasks, workers, and tags"

--- apps/task_manager/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class TaskManagerConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.task_manager"
-
-    def ready(self):
-        import apps.task_manager.signals
